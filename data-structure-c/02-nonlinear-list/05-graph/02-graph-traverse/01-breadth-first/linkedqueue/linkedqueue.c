b8096aca407b7ed6d582f1be9a7d4829fc1b067e
hzm-January <hzm_dream@163.com>
2019-08-19 21:21:56 +0800

    traverse all vertices in the graph with breadth-first

---
+++ data-structure-c/02-nonlinear-list/05-graph/02-graph-traverse/01-breadth-first/linkedqueue/linkedqueue.c
@@ -0,0 +1,88 @@
+//
+// Created by hzm on 2019/6/18.
+//
+#include "linkedqueue.h"
+
+void initializeQueue();
+
+void destroyQueue();
+
+void initializeQueue() {
+    queue = (Queue *) malloc(sizeof(Queue));
+    queue->size = 0;
+    queue->sentinel = (LinkNode *) malloc(sizeof(LinkNode));
+    queue->sentinel->value = -1;
+    queue->sentinel->next = NULL;
+}
+
+void destroyQueue() {
+    if (queue != NULL) {
+        free(queue);
+        queue = NULL;
+    }
+}
+
+void enQueue(int value) {
+    isFullQueue();
+    LinkNode *newNode = (LinkNode *) malloc(sizeof(LinkNode));
+    newNode->value = value;
+    newNode->next = NULL;
+    LinkNode *p = queue->sentinel;
+    while (p->next != NULL) {
+        p = p->next;
+    }
+    p->next = newNode;
+    queue->size++;
+}
+
+int deQueue() {
+    isEmptyQueue();
+    LinkNode *delNode = queue->sentinel->next;
+    queue->sentinel->next = delNode->next;
+    int delVal = delNode->value;
+    freeNode(delNode);
+    queue->size--;
+    return delVal;
+}
+
+int getFront() {
+    isEmptyQueue();
+    return queue->sentinel->next->value;
+}
+
+bool isEmptyQueue() {
+    if (queue->size == 0) {
+        printf(" the queue is empty ...");
+        exit(0);
+    }
+}
+
+bool isFullQueue() {
+    if (queue->size == MAX_SIZE) {
+        printf("the queue is full");
+        exit(0);
+    }
+}
+
+int getSize() {
+    return queue->size;
+}
+
+void freeNode(LinkNode *delNode) {
+    if (delNode != NULL) {
+        free(delNode);
+        delNode = NULL;
+    }
+}
+
+void output() {
+    printf("start|\t");
+    LinkNode *p = queue->sentinel->next;
+    int i = 0;
+    while (p != NULL) {
+        printf("%d|%d\t", i++, p->value);
+        p = p->next;
+    }
+    printf("|end\n");
+}
+
---
+++ data-structure-c/02-nonlinear-list/05-graph/02-graph-traverse/01-breadth-first/linkedqueue/linkedqueue.h
@@ -0,0 +1,45 @@
+//
+// Created by hzm on 2019/6/18.
+//
+
+#ifndef DATA_STRUCTURE_C_LINKEDQUEUE_H
+#define DATA_STRUCTURE_C_LINKEDQUEUE_H
+
+#define MAX_SIZE 10
+
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct LINK_NODE_STRUCT {
+    int value;
+    struct LINK_NODE_STRUCT *next;
+} LinkNode;
+typedef struct QUEUE_LINKED {
+    LinkNode *sentinel;
+    int size;
+} Queue;
+Queue *queue;
+
+void enQueue(int value);
+
+int deQueue();
+
+int getFront();
+
+bool isEmptyQueue();
+
+bool isFullQueue();
+
+int getSize();
+
+void output();
+
+void freeNode(LinkNode *delNode);
+
+void initializeQueue();
+
+void destroyQueue();
+
+#endif //DATA_STRUCTURE_C_LINKEDQUEUE_H
